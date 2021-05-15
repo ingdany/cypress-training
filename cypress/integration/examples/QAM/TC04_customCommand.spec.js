3f8dda0e98dd9899e3325ef8708d9ad9cc4c66b8
Daniel Perez <ing.dany@gmail.com>
2021-05-15 11:37:48 -0700

    Uploading TC04

---
+++ cypress/integration/examples/QAM/TC04_customCommand.spec.js
@@ -0,0 +1,31 @@
+describe("Create and mark-unmark as favorite", function () {
+
+    Cypress.config('pageLoadTimeout', 10000)
+
+    before(function () {
+        cy.SignIn()
+    });
+
+    it("Create a post", function () {
+        //cy.contains("New Post").click();
+        cy.get('ul.navbar-nav').children().contains('New Post').click()
+        cy.hash().should("include", "#/editor");
+        cy.get('form').within(($form) => {
+            cy.get('input').first().type("Test");
+            cy.get('input').eq(1).type("Test 1");
+            cy.get('textarea').last().type("Test 2");            
+            cy.contains("Publish Article").click();
+        })        
+        cy.url().should("include", "article");
+    });
+
+    it("Mark-unmark as favorite", function () {
+        //cy.get(".nav-link").contains("dperez").click();
+        cy.get("ul.navbar-nav").children().contains("dperez").click();
+        cy.contains("My Articles").should("be.visible");
+        cy.get(".ion-heart").first().click();
+        cy.contains("Favorited Articles").click();
+        cy.url().should("include", "favorites");
+        cy.get(".ion-heart").first().click();        
+    });
+});
